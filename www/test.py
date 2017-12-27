cb581b5e64c565e273d93288b43aa916f37b1ef0
Won <wenwenwen888@live.com>
2017-12-27 18:33:19 +0800

    init commit

---
+++ www/test.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+import asyncio
+
+import sys
+
+from www import orm
+from www.model import User
+
+__author__ = 'Won'
+__date__ = '2017/12/26 16:51'
+
+
+@asyncio.coroutine
+def test(loop):
+    yield from orm.create_pool(loop=loop, user='root', password='3687014', db='awesome')
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+    yield from u.save()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.close()
+if loop.is_closed():
+    sys.exit(0)
