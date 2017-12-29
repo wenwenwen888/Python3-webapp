9d7fa8131cb566a5fadaf0da04ccee407dba2b1d
Won <wenwenwen888@live.com>
2017-12-29 10:11:46 +0800

    init day 9

--- www/handlers.py
+++ www/handlers.py
@@ -1,5 +1,34 @@
 # -*- coding:utf-8 -*-
+import asyncio
+import time
+
+from www.coroweb import get
+from www.model import User, Blog
+
 __author__ = 'Won'
 __date__ = '2017/12/28 11:58'
 
 ' url handlers '
+
+
+@get('/')
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,' \
+              ' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
+    ]
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs
+    }
+
+
+@get('/api/users')
+async def api_get_users():
+    users = await User.findAll(orderBy='created_at desc')
+    for u in users:
+        u.passwd = '******'
+    return dict(users=users)
--- www/test.py
+++ www/test.py
@@ -13,7 +13,7 @@ __date__ = '2017/12/26 16:51'
 @asyncio.coroutine
 def test(loop):
     yield from orm.create_pool(loop=loop, user='root', password='3687014', db='awesome')
-    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+    u = User(name='Won', email='wenjunnan1994@gmail.com', passwd='1234567890', image='about:blank')
     yield from u.save()
 
 
